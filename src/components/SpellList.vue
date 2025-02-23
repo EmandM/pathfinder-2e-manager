38bbaa715e32c05ab90e325afcca756010faddbc
Pitimoi <pitimoi@pm.me>
2025-02-23 17:44:08 -0500

    added markdown and filter

--- src/components/SpellList.vue
+++ src/components/SpellList.vue
@@ -1,13 +1,21 @@
 <script lang="ts" setup>
 import { repository } from '~/../package.json'
 
+import markdownit from 'markdown-it'
+const md = markdownit({html: true, breaks: true})
+
 import spells from '~/assets/data/spell.json'
+
+function GetSpells() {
+  return spells.filter(s => s._source.primary_source == "Player Core");
+}
+
 </script>
 
 <template>
   <div class="spells">
-    <div v-for="spell in spells">
-      {{spell._source.markdown}}
+    <div v-for="spell in GetSpells()">
+      <span v-html="md.render(spell._source.markdown)" />
     </div>
   </div>
 </template>
