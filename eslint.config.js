d5c588b99d195a4ba16b72610ba752ee3c5aaa5f
EmandM <loopylemon8@gmail.com>
2025-11-28 01:04:36 -0500

    update eslint file

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   unocss: true,
+  vue: true,
   typescript: {
     tsconfigPath: 'tsconfig.json',
-    overrides: {
-      'ts/strict-boolean-expressions': [0],
-    },
-  },
-  vue: {
-    overrides: {
-      'vue/no-mutating-props': ['error', {
-        shallowOnly: true,
-      }],
-      'vue/singleline-html-element-content-newline': [0],
-    },
   },
 }, {
   ignores: [
@@ -25,7 +15,17 @@ export default antfu({
     '.vscode/**',
   ],
 }, {
+  // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
+  files: ['**/*.vue'],
+  rules: {
+    'vue/no-mutating-props': ['error', {
+      shallowOnly: true,
+    }],
+    'vue/singleline-html-element-content-newline': [0],
+  },
+}, {
   rules: {
     'no-console': [0],
+    'ts/strict-boolean-expressions': [0],
   },
 })
